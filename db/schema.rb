f3880ce159f4bccea8526f2b18f4e4789b019419
Chris Gullickson <cgullickson5@gmail.com>
2018-02-09 01:27:43 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,6 @@ ActiveRecord::Schema.define(version: 20180209011301) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
-    t.integer  "patient_id"
-    t.integer  "doctor_id"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
   end
